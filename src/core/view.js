d540f2c79282d8aec0cd3bd0f575ae376a654338
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2023-10-15 02:58:46 +0200

    feat: created moveFigure & rotateFigure

--- src/core/view.js
+++ src/core/view.js
@@ -4,6 +4,7 @@ class View {
   constructor(game, container) {
     this.container = container;
     this.game = game;
+    this.currentFigure = this.addFigure();
   }
 
   addFigure() {
@@ -32,17 +33,72 @@ class View {
       });
     });
 
-
-
-    this.container.append(figureContainer);
-    console.dir(this.container)
-    console.log(figure);
-
     return figureContainer;
   }
 
+  rotateFigure() {
+    this.currentFigure.innerHTML = '';
+
+    const { figure, x, y } = this.game.currentFigure;
+    const { field } = this.game;
+    const width = this.container.offsetWidth / field[0].length;
+    const height = this.container.offsetHeight / field.length;
+
+    figure.forEach((line, lineIndex) => {
+      line.forEach((columItem, columIndex) => {
+        const item = createElement({ tagName: 'div', className: columItem ? 'item' : '' });
+
+        const left = width * columIndex;
+        const top = height * lineIndex;
+
+        item.style.width = `${width}px`;
+        item.style.height = `${height}px`;
+        item.style.left = `${left}px`;
+        item.style.top = `${top}px`;
+
+        this.currentFigure.append(item);
+      });
+    });
+  }
+
+  moveFigure() {
+    const { field } = this.game
+    const width = this.container.offsetWidth / field[0].length;
+    const height = this.container.offsetHeight / field.length;
+
+    document.onkeydown = (e) => {
+      console.log(e)
+      if ( e.key === 'ArrowDown') {
+        const { moveFigure } = this.game;
+        moveFigure('down');
+        this.currentFigure.style.top = `${this.game.currentFigure.y * height}px`;
+      }
+      if ( e.key === 'ArrowLeft') {
+        const { moveFigure } = this.game;
+        moveFigure('left');
+        this.currentFigure.style.left = `${this.game.currentFigure.x * width}px`;
+      }
+      if ( e.key === 'ArrowRight') {
+        const { moveFigure } = this.game;
+        moveFigure('right');
+        this.currentFigure.style.left = `${this.game.currentFigure.x * width}px`;
+      }
+      if ( e.key === 'ArrowUp') {
+        const { rotateFigure } = this.game;
+
+        rotateFigure();
+        this.rotateFigure();
+
+        this.currentFigure.style.top = `${this.game.currentFigure.y * height}px`;
+        this.currentFigure.style.left = `${this.game.currentFigure.x * width}px`;
+      }
+    }
+  }
+
   run() {
-    this.addFigure();
+    this.container.append(this.currentFigure);
+    this.moveFigure();
+    console.log(this.currentFigure)
   }
 }
 
